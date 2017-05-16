5b894e3eac1a01a219bb779a631e36ba559b8b08
wangyikai1234567 <2285281993@qq.com>
2017-05-16 14:09:23 +0800

    加个注释

--- app/src/main/java/com/redbaby/MainActivity.java
+++ app/src/main/java/com/redbaby/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        王艺凯加个注释侧测试
     }
 }
